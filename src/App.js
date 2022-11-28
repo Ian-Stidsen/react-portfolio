41bd90551aca51249d06f0600808e826d9ebbd03
Ian Stidsen <ianlundstidsen@gmail.com>
2022-11-28 15:42:02 -0300

    added functionality to contact form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useRef } from 'react'
+import emailjs from '@emailjs/browser';
+
 import './App.css';
 import LLRegnskab from './img/ll-regnskab.jpg';
 
@@ -28,8 +30,29 @@ function App() {
     }
   }
 
-  function sendMail() {
-    console.log('sent');
+  const serviceId = 'portfolio-service',
+    templateId = 'portfolio-template',
+    publicKey = '8nesMxDbdPp8ZsJoH';
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        serviceId,
+        templateId,
+        form.current,
+        publicKey
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        }, 
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
   return (
     <div className="App">
@@ -119,53 +142,62 @@ function App() {
           </div>
         </section>
 
-        <section className="md-4" ref={contactRef}>
-          <h2 className="h1-responsive font-weight-bold text-center my-4">Contact me</h2>
+        <section className="contact md-4" ref={contactRef}>
+          <h2 className="h1-responsive font-weight-bold text-center my-2">Contact me</h2>
           <p className="text-center w-responsive mx-auto mb-5">
-            You can contact me here, if you have any questions.
+            You can contact me here, if you have any questions. 
+            Or you can click the links below.
           </p>
 
           <div className="row">
-            <div className="col-md-9 mb-md-0 mb-5">
-              <form id="contact-form" name="contact-form" action="mail.php" method="POST">
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="md-form mb-0">
-                      <label htmlFor="name" className="">Your name</label>
-                      <input type="text" id="name" name="name" className="form-control"></input>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="md-form mb-0">
-                      <label htmlFor="email" className="">Your email</label>
-                      <input type="text" id="email" name="email" className="form-control"></input>
+            <div className="col-12 mb-5">
+              <form ref={form} id="contact-form" name="contact-form" action="mail.php" method="POST">
+              <div className="row">
+                  <div className="col-12">
+                    <div className="md-form mb-1">
+                      <div className='input-group'>
+                        <div className='input-group-text'>Name</div>
+                        <input type="text" id="name" name="user_name" className="form-control" placeholder='Your name'></input>
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div className="row">
-                  <div className="col-md-12">
-                    <div className="md-form mb-0">
-                      <label htmlFor="subject" className="">Subject</label>
-                      <input type="text" id="subject" name="subject" className="form-control"></input>
+                  <div className="col-12">
+                    <div className="md-form mb-1">
+                      <div className='input-group'>
+                        <div className='input-group-text'>@</div>
+                        <input type="text" id="email" name="user_email" className="form-control" placeholder='Your email'></input>
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div className="row">
-                  <div className="col-md-12">
+                  <div className="col-12 mb-3">
                     <div className="md-form">
-                      <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea"></textarea>                          <label for="message">Your message</label>
+                      <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea" placeholder='Message'></textarea>
                     </div>
                   </div>
                 </div>
               </form>
 
               <div className="text-center text-md-left">
-                <button className="btn btn-primary" onClick={sendMail}>Send</button>
+                <button className="btn btn-primary" onClick={sendEmail}>Send</button>
               </div>
               <div className="status"></div>
             </div>
-            <div className="col-md-3 text-center">
+            <div className="col text-center">
               <ul className="list-unstyled mb-0">
+                <li>
+                  <a href='https://github.com/Ian-Stidsen' target='_blank' rel='noopener noreferrer'>
+                    Github: Ian Stidsen
+                  </a>
+                </li>
+                <li>
+                  <a href='https://www.linkedin.com/in/ian-lund-stidsen-02aa51248/' target='_blank' rel='noopener noreferrer'>
+                    LinkedIn: Ian Lund Stidsen
+                  </a>
+                </li>
                 <li>
                   <a href='tel:+299244103'>Mobile: (+299)244103</a>
                 </li>
