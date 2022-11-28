6470d4d62a3adf928392180f77db79dd0f168fb6
Ian Stidsen <ianlundstidsen@gmail.com>
2022-11-28 09:34:28 -0300

    reac-todo

---
+++ src/portfolio/CurrencyCalculator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CurrencyCalculator() {
+  return (
+    <div>
+      CurrencyCalculator
+    </div>
+  )
+};
+
+export default CurrencyCalculator;
---
+++ src/portfolio/Speedtype.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Speedtype() {
+  return (
+    <div>
+      Speedtype
+    </div>
+  )
+};
+
+export default Speedtype;
