6488af14bcfd0e58f4b99d32b5b5ee9ab1c1553e
Ian Stidsen <ianlundstidsen@gmail.com>
2022-12-06 21:34:17 -0300

    cleaned up dir

--- src/App.js
+++ src/App.js
@@ -2,17 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Portfolio from './pages/Portfolio/Portfolio';
-import CurrencyCalculator from './pages/Projects/CurrencyCalculator';
-import TodoList from './pages/Projects/TodoList';
-import Speedtype from './pages/Projects/Speedtype';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Portfolio />} />
-        <Route path="/currency-calculator" element={<CurrencyCalculator />} />
-        <Route path="/todo-list" element={<TodoList />} />
-        <Route path="/speedtype" element={<Speedtype />} />
       </Routes>
     </BrowserRouter>
   )
